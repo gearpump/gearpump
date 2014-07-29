b19de56fe87c8989be4f7111bed41e80d0402416
Sean Zhong <clockfly@gmail.com>
2014-07-29 11:30:25 +0800

    fix script

--- bin/wordcount.sh
+++ bin/wordcount.sh
@@ -3,4 +3,4 @@ ip=$1
 port=$2
 
 echo java -cp "lib/*" org.apache.gearpump.app.examples.wordcount.WordCount $ip $port
-java -cp "lib/*" org.apache.gearpump.app.examples.wordcount.WordCount $ip $por
+java -cp "lib/*" org.apache.gearpump.app.examples.wordcount.WordCount $ip $port
