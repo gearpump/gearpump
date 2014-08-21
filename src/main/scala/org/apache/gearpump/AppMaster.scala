48ad268b17fd144ca2e2ab17d86e7da3457e46c4
Kam Kasravi <kamkasravi@yahoo.com>
2014-08-21 15:48:42 -0700

    Provide a WorkerExectorContext which can add extra jvm opts

--- src/main/scala/org/apache/gearpump/AppMaster.scala
+++ src/main/scala/org/apache/gearpump/AppMaster.scala
@@ -85,7 +85,10 @@ class AppMaster (config : Configs) extends Actor {
         val (worker, slots) = workerAndSlots
         LOG.info(s"Launching Executor ...appId: $appId, executorId: $currentExecutorId, slots: $slots on worker $worker")
         val executorConfig = appDescription.conf.withAppId(appId).withAppMaster(self).withExecutorId(currentExecutorId).withSlots(slots)
-        worker ! LaunchExecutor(appId, currentExecutorId, slots,  classOf[Executor], executorConfig, new DefaultExecutorContext)
+        val workerContext = new WorkerExecutorContext(context.system.settings.config.getString("gearpump.worker.jvmargs"))
+        val jvmargs = workerContext.getJvmArguments().mkString(" ")
+        LOG.info(s"args = $jvmargs")
+        worker ! LaunchExecutor(appId, currentExecutorId, slots,  classOf[Executor], executorConfig, workerContext)
         //TODO: Add timeout event if this executor fail to start
         currentExecutorId += 1
       })
