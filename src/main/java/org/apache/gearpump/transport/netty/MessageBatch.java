90111b1c17e0e47da35f85ef86967e4505578cf9
Sean Zhong <clockfly@gmail.com>
2014-09-04 00:07:14 +0800

    update copyrights.

--- src/main/java/org/apache/gearpump/transport/netty/MessageBatch.java
+++ src/main/java/org/apache/gearpump/transport/netty/MessageBatch.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.gearpump.transport.netty;
 
 import org.jboss.netty.buffer.ChannelBuffer;
