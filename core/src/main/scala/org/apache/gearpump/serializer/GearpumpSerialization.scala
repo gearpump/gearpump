55a437ebafe492213caa25a0a1cf9d6fa8007c7d
Sean Zhong <clockfly@gmail.com>
2014-10-13 16:20:53 +0800

    add default configs

--- core/src/main/scala/org/apache/gearpump/serializer/GearpumpSerialization.scala
+++ core/src/main/scala/org/apache/gearpump/serializer/GearpumpSerialization.scala
@@ -18,7 +18,12 @@
 
 package org.apache.gearpump.serializer
 
-import com.esotericsoftware.kryo.{Kryo, Serializer}
+import java.math.BigInteger
+import java.util
+import java.util._
+
+import akka.actor.ActorRef
+import com.esotericsoftware.kryo.{KryoSerializable, Kryo, Serializer}
 import org.apache.gearpump.util.{Configs, Constants}
 import org.slf4j.{LoggerFactory, Logger}
 
@@ -30,7 +35,59 @@ class GearpumpSerialization {
 
     LOG.info("GearpumpSerialization init........")
 
-    val serializationMap: Map[String, String] = configToMap(Constants.GEARPUMP_SERIALIZERS)
+    val serializationMap = configToMap(Constants.GEARPUMP_SERIALIZERS)
+
+    kryo.register(classOf[Array[Byte]])
+    kryo.register(classOf[Array[Char]])
+    kryo.register(classOf[Array[Short]])
+    kryo.register(classOf[Array[Int]])
+    kryo.register(classOf[Array[Long]])
+    kryo.register(classOf[Array[Float]])
+    kryo.register(classOf[Array[Double]])
+    kryo.register(classOf[Array[Boolean]])
+    kryo.register(classOf[Array[String]])
+
+    kryo.register(classOf[Array[Object]])
+
+    kryo.register(classOf[BigInteger])
+    kryo.register(classOf[BigDecimal])
+    kryo.register(classOf[Class[Any]])
+    kryo.register(classOf[Date])
+    kryo.register(classOf[Enum[Any]])
+    kryo.register(classOf[util.EnumSet[Any]])
+    kryo.register(classOf[Currency])
+    kryo.register(classOf[StringBuffer])
+    kryo.register(classOf[StringBuilder])
+
+    kryo.register(Collections.EMPTY_LIST.getClass())
+    kryo.register(Collections.EMPTY_MAP.getClass())
+    kryo.register(Collections.EMPTY_SET.getClass())
+    kryo.register(Collections.singletonList(null).getClass())
+    kryo.register(Collections.singletonMap(null, null).getClass())
+    kryo.register(Collections.singleton(null).getClass())
+
+    kryo.register(classOf[util.TreeSet[_]])
+    kryo.register(classOf[util.Collection[_]])
+    kryo.register(classOf[util.TreeMap[_, _]])
+    kryo.register(classOf[Map[_, _]])
+    kryo.register(classOf[TimeZone])
+    kryo.register(classOf[Calendar])
+    kryo.register(classOf[Locale])
+
+    kryo.register(classOf[scala.Enumeration#Value])
+    kryo.register(classOf[scala.collection.Map[_,_]])
+    kryo.register(classOf[scala.collection.Set[_]])
+    kryo.register(classOf[scala.collection.generic.MapFactory[scala.collection.Map]])
+    kryo.register(classOf[scala.collection.generic.SetFactory[scala.collection.Set]])
+    kryo.register(classOf[scala.collection.Traversable[_]])
+    kryo.register(classOf[ActorRef])
+
+    kryo.register(classOf[scala.Tuple1[_]])
+    kryo.register(classOf[scala.Tuple2[_,_]])
+    kryo.register(classOf[scala.Tuple3[_,_,_]])
+    kryo.register(classOf[scala.Tuple4[_,_,_,_]])
+    kryo.register(classOf[scala.Tuple5[_,_,_,_,_]])
+    kryo.register(classOf[scala.Tuple6[_,_,_,_,_,_]])
 
     serializationMap.foreach { kv =>
       val (key, value) = kv
@@ -41,7 +98,7 @@ class GearpumpSerialization {
     kryo.setReferences(false)
   }
 
-  private final def configToMap(path: String): Map[String, String] = {
+  private final def configToMap(path: String) = {
     import scala.collection.JavaConverters._
     config.getConfig(path).root.unwrapped.asScala.toMap map { case (k, v) ⇒ (k -> v.toString) }
   }
