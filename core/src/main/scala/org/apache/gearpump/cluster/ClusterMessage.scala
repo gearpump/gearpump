e7d58070c48fb5562620f897b3e0f7f78301b68d
huafengw <fvunicorn@gmail.com>
2014-11-07 10:12:30 +0800

    add PriorityScheduler test case and minor bug fix

--- core/src/main/scala/org/apache/gearpump/cluster/ClusterMessage.scala
+++ core/src/main/scala/org/apache/gearpump/cluster/ClusterMessage.scala
@@ -63,7 +63,16 @@ object AppMasterToMaster {
 }
 
 object MasterToAppMaster {
-  case class ResourceAllocated(allocations: Array[ResourceAllocation])
+  case class ResourceAllocated(allocations: Array[ResourceAllocation]){
+    override def equals(other: Any): Boolean = {
+      other match {
+        case that: ResourceAllocated =>
+          allocations.sameElements(that.allocations)
+        case _ =>
+          false
+      }
+    }
+  }
   case class AppMasterRegistered(appId: Int, master : ActorRef)
   case object ShutdownAppMaster
   case class AppMasterData(appId: Int, appData: AppMasterInfo)
