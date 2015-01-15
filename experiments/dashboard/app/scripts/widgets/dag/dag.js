e00f122c10aa3deb1e0c9ebd2844c9c2fa9b1e18
Kam Kasravi <kamkasravi@yahoo.com>
2015-01-15 08:59:28 -0800

    Fixes #269

---
+++ experiments/dashboard/app/scripts/widgets/dag/dag.js
@@ -0,0 +1,79 @@
+/*
+ * The MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+'use strict';
+
+angular.module('app.widgets.dag', ['adf.provider', 'nvd3'])
+  .config(function(dashboardProvider){
+    dashboardProvider
+      .widget('dag', {
+        title: 'Dag',
+        description: 'Dag widget',
+        controller: 'dagCtrl',
+        templateUrl: 'scripts/widgets/dag/dag.html',
+        edit: {
+          templateUrl: 'scripts/widgets/dag/edit.html',
+          reload: false
+        }
+      });
+  })
+  .controller('dagCtrl', function($scope){
+    $scope.options = {
+        chart: {
+            type: 'discreteBarChart',
+            height: 450,
+            margin : {
+                top: 20,
+                right: 20,
+                bottom: 60,
+                left: 55
+            },
+            x: function(d){ return d.label; },
+            y: function(d){ return d.value; },
+            showValues: true,
+            valueFormat: function(d){
+                return d3.format(',.4f')(d);
+            },
+            transitionDuration: 500,
+            xAxis: {
+                axisLabel: 'X Axis'
+            },
+            yAxis: {
+                axisLabel: 'Y Axis',
+                axisLabelDistance: 30
+            }
+        }
+    };
+    $scope.data = [{
+        key: "Cumulative Return",
+        values: [
+            { "label" : "A" , "value" : -29.765957771107 },
+            { "label" : "B" , "value" : 0 },
+            { "label" : "C" , "value" : 32.807804682612 },
+            { "label" : "D" , "value" : 196.45946739256 },
+            { "label" : "E" , "value" : 0.19434030906893 },
+            { "label" : "F" , "value" : -98.079782601442 },
+            { "label" : "G" , "value" : -13.925743130903 },
+            { "label" : "H" , "value" : -5.1387322875705 }
+            ]
+        }];
+  });
