2d0659b3a1cdfae2ca868f2cb1eb0c0c51e7cabb
Kam Kasravi <kamkasravi@yahoo.com>
2015-01-23 16:45:33 -0800

    Fixes #362. Increased PROCESS_BOOT_TIME and added system.awaitTermination

--- core/src/test/scala/org/apache/gearpump/cluster/MasterHarness.scala
+++ core/src/test/scala/org/apache/gearpump/cluster/MasterHarness.scala
@@ -42,7 +42,7 @@ trait MasterHarness {
   private var systemAddress: Address = null
   private var host: String = null
   private var port: Int = 0
-  val PROCESS_BOOT_TIME = Duration(10, TimeUnit.SECONDS)
+  val PROCESS_BOOT_TIME = Duration(15, TimeUnit.SECONDS)
 
   def getActorSystem: ActorSystem = system
   def getHost: String = host
@@ -61,6 +61,7 @@ trait MasterHarness {
 
   def shutdownActorSystem():Unit = {
     system.shutdown()
+    system.awaitTermination
     LOG.info(s"Actor system is stopped, $host, $port")
   }
 
