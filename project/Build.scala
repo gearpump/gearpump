dc8be7ae4d4cd4351afbf41c80c8d007e25d21e0
Sean Zhong <clockfly@gmail.com>
2015-03-24 00:35:11 +0800

    fix #650, remove logback classic slf4j binding from classpath

--- project/Build.scala
+++ project/Build.scala
@@ -352,7 +352,8 @@ object Build extends sbt.Build {
             exclude("ring", "ring-core")
             exclude("ring", "ring-devel")
             exclude("ring", "ring-jetty-adapter")
-            exclude("ring", "ring-servlet"),
+            exclude("ring", "ring-servlet")
+            exclude("ch.qos.logback", "logback-classic"),
           "org.apache.storm" % "storm-starter" % stormVersion,
           "org.scalatest" %% "scalatest" % scalaTestVersion % "test",
           "org.scalacheck" %% "scalacheck" % scalaCheckVersion % "test",
