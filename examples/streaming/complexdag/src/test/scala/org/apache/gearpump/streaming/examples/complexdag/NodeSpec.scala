c3740d65b1c84d06d1ed827463c19a14ac0e3746
josh <josh@protectwise.com>
2015-04-09 16:59:21 -0600

    issue #799 Getting 2.10 cross build working

--- examples/streaming/complexdag/src/test/scala/org/apache/gearpump/streaming/examples/complexdag/NodeSpec.scala
+++ examples/streaming/complexdag/src/test/scala/org/apache/gearpump/streaming/examples/complexdag/NodeSpec.scala
@@ -33,9 +33,9 @@ class NodeSpec extends PropSpec with PropertyChecks with Matchers with BeforeAnd
 
   val node = new Node(context, UserConfig.empty)
 
-  property("Node should send a List[String](classOf[Node].getCanonicalName, classOf[Node].getCanonicalName"){
-    val list = List(classOf[Node].getCanonicalName)
-    val expected = List(classOf[Node].getCanonicalName,classOf[Node].getCanonicalName)
+  property("Node should send a Array[String](classOf[Node].getCanonicalName, classOf[Node].getCanonicalName"){
+    val list = Array(classOf[Node].getCanonicalName)
+    val expected = Array(classOf[Node].getCanonicalName,classOf[Node].getCanonicalName)
     node.onNext(Message(list))
     verify(context).output(argMatch[Message](_.msg == expected))
   }
