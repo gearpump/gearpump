98d3f0376514154b03cbbdbf0d1e8e90974d4d8d
Kam Kasravi <kamkasravi@yahoo.com>
2015-05-27 11:42:39 -0700

    Fixes #959 LOG in TimeoutScheduler causes JVM exit

--- core/src/main/scala/org/apache/gearpump/util/TimeOutScheduler.scala
+++ core/src/main/scala/org/apache/gearpump/util/TimeOutScheduler.scala
@@ -27,13 +27,11 @@ import scala.concurrent.duration._
 trait TimeOutScheduler {
   this: Actor =>
   import context.dispatcher
-  private val log = LogUtil.getLogger(getClass)
 
   def sendMsgWithTimeOutCallBack(target: ActorRef, msg: AnyRef, seconds: Int, timeOutHandler: => Unit): Unit = {
     val result = target.ask(msg)(FiniteDuration(seconds, TimeUnit.SECONDS))
     result onSuccess {
       case msg =>
-        log.info(s"sending $msg to ${self.path.toString}")
         self ! msg
     }
     result onFailure {
