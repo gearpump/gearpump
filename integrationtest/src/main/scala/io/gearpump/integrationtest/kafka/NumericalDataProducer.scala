809b5bfcec60a3487c474b05878d483359e6d94f
huafengw <fvunicorn@gmail.com>
2015-11-20 17:58:24 +0800

    add at-least-once test

---
+++ integrationtest/src/main/scala/io/gearpump/integrationtest/kafka/NumericalDataProducer.scala
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gearpump.integrationtest.kafka
+
+import java.util.Properties
+
+import com.twitter.bijection.Injection
+import org.apache.kafka.clients.producer.{ProducerRecord, KafkaProducer}
+import org.apache.kafka.common.serialization.ByteArraySerializer
+import org.apache.log4j.Logger
+
+class NumericalDataProducer(topic: String, bootstrapServers: String) {
+  private val LOG = Logger.getLogger(getClass)
+  private val producer = createProducer
+  private val WRITE_SLEEP_NANOS = 10
+  var lastWriteNum = 0
+
+  def start(): Unit = {
+    produceThread.start()
+  }
+
+  def stop(): Unit= {
+    if(produceThread.isAlive) {
+      produceThread.interrupt()
+      produceThread.join()
+    }
+    producer.close()
+  }
+
+  private def createProducer: KafkaProducer[Array[Byte], Array[Byte]] = {
+    val properties = new Properties()
+    properties.setProperty("bootstrap.servers", bootstrapServers)
+    new KafkaProducer[Array[Byte], Array[Byte]](properties, new ByteArraySerializer, new ByteArraySerializer)
+  }
+
+  private val produceThread = new Thread(new Runnable {
+    override def run(): Unit = {
+      try{
+        while (!Thread.currentThread.isInterrupted) {
+          lastWriteNum += 1
+          val msg = Injection[String, Array[Byte]](lastWriteNum.toString)
+          val record = new ProducerRecord[Array[Byte], Array[Byte]](topic, msg)
+          producer.send(record)
+          Thread.sleep(0, WRITE_SLEEP_NANOS)
+        }
+      } catch {
+        case e: InterruptedException => LOG.info("fetch thread got interrupted exception")
+      }
+    }
+  })
+}
---
+++ integrationtest/src/main/scala/io/gearpump/integrationtest/kafka/SimpleKafkaReader.scala
@@ -0,0 +1,28 @@
+package io.gearpump.integrationtest.kafka
+
+import com.twitter.bijection.Injection
+import kafka.api.FetchRequestBuilder
+import kafka.consumer.SimpleConsumer
+import kafka.utils.Utils
+
+import scala.util.{Failure, Success}
+
+trait ResultVerifier {
+  def onNext(msg: String): Unit
+}
+
+class SimpleKafkaReader(val msgReceiver: ResultVerifier, topic: String, partition: Int = 0, host: String, port: Int) {
+  private val consumer = new SimpleConsumer(host, port, 100000, 64 * 1024, "")
+  private val iterator = consumer.fetch(new FetchRequestBuilder().addFetch(topic, partition, 0, Int.MaxValue).build())
+    .messageSet(topic, 0).iterator
+
+  def read(): Unit = {
+    while (iterator.hasNext) {
+      Injection.invert[String, Array[Byte]](Utils.readBytes(iterator.next().message.payload)) match {
+        case Success(msg) =>
+          msgReceiver.onNext(msg)
+        case Failure(e) => throw e
+      }
+    }
+  }
+}
