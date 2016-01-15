2a57fcbe6a3003424b82adf4540fd2c8c00817ed
Qian Xu <qian.a.xu@intel.com>
2016-01-15 15:23:45 +0800

    fix #1830: removed cluster side menu and tuned main navbar

--- services/dashboard/views/landing/header.js
+++ services/dashboard/views/landing/header.js
@@ -13,29 +13,19 @@ angular.module('dashboard')
       templateUrl: 'views/landing/header.html',
       replace: true,
       scope: {},
-      controller: ['$scope', '$cookies', 'restapi', 'conf',  function($scope, $cookies, restapi, conf) {
-        $scope.menu = [
-          {text: 'Cluster', pathPatt: '/cluster', href: '#/cluster', icon: 'glyphicon glyphicon-th-large'},
-          {text: 'Applications', pathPatt: '/apps', href: '#/apps', icon: 'glyphicon glyphicon-tasks'}
+      controller: ['$scope', '$cookies', 'restapi', 'conf', function($scope, $cookies, restapi, conf) {
+        $scope.clusterMenuItems = [
+          {text: 'Master', pathPatt: 'master', icon: 'fa fa-opera'},
+          {text: 'Worker Instances', pathPatt: 'workers', icon: 'fa fa-server'}
         ];
 
-        var username = $cookies.get('username');
-        $scope.username = username;
-
-        $scope.loginUrl = conf.loginUrl;
-
-        $scope.links = [
+        $scope.username = $cookies.get('username');
+        $scope.userMenuItems = [
+          {text: 'Sign Out', href: conf.loginUrl, icon: 'glyphicon glyphicon-off'},
+          {isDivider: true},
           {text: 'Documents', href: '//gearpump.io', icon: 'fa fa-book'},
           {text: 'GitHub', href: '//github.com/gearpump/gearpump', icon: 'fa fa-github'}
         ];
-
-        $scope.dropdownMenuOptions = ([].concat($scope.menu).concat($scope.links))
-          .map(function(item) {
-            return {
-              text: '<i class="' + item.icon + '"></i> ' + item.text,
-              href: item.href
-            };
-          });
 
         $scope.version = 'beta';
         restapi.serviceVersion(function(version) {
