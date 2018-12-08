9aff68e7acdc98d3e1a803768c646f59b75e4fac
manuzhang <owenzhang1990@gmail.com>
2018-12-08 14:45:17 +0800

    Upgrade sbt-unidoc to 0.4.2

--- project/Docs.scala
+++ project/Docs.scala
@@ -15,27 +15,26 @@
 import BuildGearpump.{core, streaming}
 import sbt.Keys._
 import sbt._
-import sbtunidoc.Plugin.UnidocKeys._
-import sbtunidoc.Plugin._
+import sbtunidoc.BaseUnidocPlugin.autoImport.{unidoc, unidocAllSources, unidocProjectFilter}
+import sbtunidoc.JavaUnidocPlugin.autoImport.JavaUnidoc
+import sbtunidoc.ScalaUnidocPlugin.autoImport.ScalaUnidoc
 
 object Docs extends sbt.Build {
   lazy val javadocSettings = Seq(
-    addCompilerPlugin(
-      "com.typesafe.genjavadoc" %% "genjavadoc-plugin" % "0.11" cross CrossVersion.full),
     scalacOptions += s"-P:genjavadoc:out=${target.value}/java"
   )
 
-  lazy val gearpumpUnidocSetting = scalaJavaUnidocSettings ++ Seq(
+  lazy val gearpumpUnidocSetting = Seq(
     unidocProjectFilter in(ScalaUnidoc, unidoc) := projectsWithDoc,
     unidocProjectFilter in(JavaUnidoc, unidoc) := projectsWithDoc,
 
     unidocAllSources in(ScalaUnidoc, unidoc) := {
-      ignoreUndocumentedPackages((unidocAllSources in(ScalaUnidoc, unidoc)).value)
+     ignoreUndocumentedPackages((unidocAllSources in(ScalaUnidoc, unidoc)).value)
     },
 
     // Skip class names containing $ and some internal packages in Javadocs
     unidocAllSources in(JavaUnidoc, unidoc) := {
-      ignoreUndocumentedPackages((unidocAllSources in(JavaUnidoc, unidoc)).value)
+     ignoreUndocumentedPackages((unidocAllSources in(JavaUnidoc, unidoc)).value)
     }
   )
 
