949745a681810641da481015cf634fdc76565e6e
Scala Steward <me@scala-steward.org>
2019-06-21 20:27:51 +0200

    Update bootstrap to 3.3.7

--- project/BuildDashboard.scala
+++ project/BuildDashboard.scala
@@ -35,7 +35,7 @@ object BuildDashboard {
         // angular 1.5 breaks ui-select, but we need ng-touch 1.5
         "org.webjars.npm" % "angular-touch" % "1.5.0",
         "org.webjars" % "angular-ui-router" % "0.2.15",
-        "org.webjars" % "bootstrap" % "3.3.6",
+        "org.webjars" % "bootstrap" % "3.3.7",
         "org.webjars" % "d3js" % "3.5.6",
         "org.webjars" % "momentjs" % "2.10.6",
         "org.webjars" % "lodash" % "3.10.1",
