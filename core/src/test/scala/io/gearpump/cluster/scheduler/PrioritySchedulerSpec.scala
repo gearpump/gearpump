edbc4dd855bbec1536b88c9e532dbda9b9de1b53
Manu Zhang <OwenZhang1990@gmail.com>
2026-05-21 15:43:26 +0800

    Style: add spacing in PrioritySchedulerSpec mixin list

--- core/src/test/scala/io/gearpump/cluster/scheduler/PrioritySchedulerSpec.scala
+++ core/src/test/scala/io/gearpump/cluster/scheduler/PrioritySchedulerSpec.scala
@@ -30,7 +30,7 @@ import org.scalatest.matchers.should.Matchers
 import scala.concurrent.duration._
 
 class PrioritySchedulerSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
-  with AnyWordSpecLike with Matchers with BeforeAndAfterAll{
+  with AnyWordSpecLike with Matchers with BeforeAndAfterAll {
 
   def this() = this(ActorSystem("PrioritySchedulerSpec", TestUtil.DEFAULT_CONFIG))
   val appId = 0
